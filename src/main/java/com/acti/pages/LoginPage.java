60fb7abfada263f6acb5cf18c00cac6ff491a18b
Indu <indhumathi3@gmail.com>
2020-07-31 15:53:43 -0400

    Tester A - Develop Login page class

---
+++ src/main/java/com/acti/pages/LoginPage.java
@@ -0,0 +1,46 @@
+package com.acti.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import com.acti.base.DriverScript;
+
+public class LoginPage extends DriverScript
+{
+	 @FindBy(id="username")WebElement usernametbox;
+	 @FindBy(name="pwd")WebElement pwdtbox;
+	 @FindBy(id="loginButton")WebElement loginbutton;
+	 @FindBy(xpath="//div[@class='atLogoImg']")WebElement actilogo;
+	 @FindBy(id="keepLoggedInCheckBox")WebElement checkbox;
+	 
+	 public LoginPage() {
+             PageFactory.initElements(driver,this);
+             }
+	 @Test
+	  public void validateLoginFunction(String username,String password)
+	  {
+		  usernametbox.sendKeys("username");
+		  pwdtbox.sendKeys("password");
+		  loginbutton.click();
+		  //actilogo.isDisplayed();
+		  //checkbox.isEnabled();
+	}
+	 
+	  public boolean validateactilogo()
+	  {
+		boolean logo = actilogo.isDisplayed();
+		return logo;
+	  }
+	  public boolean validatecheckbox()
+	  {
+		boolean check = checkbox.isEnabled();
+		  return check;
+	  }
+	  public void logintitle()
+	  {
+		  driver.getTitle();
+	  }
+	 
+}
