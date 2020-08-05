59bd3f3fd4c2ca3688eafd6212b4f5ffb4bdaa4f
Indu <indhumathi3@gmail.com>
2020-08-05 16:27:17 -0400

    Committed : Test create customer

--- src/test/java/com/acti/testcase/BaseTest.java
+++ src/test/java/com/acti/testcase/BaseTest.java
@@ -1,12 +1,16 @@
 package com.acti.testcase;
 
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 import com.acti.base.DriverScript;
 import com.acti.pages.EnterTimePage;
 import com.acti.pages.LoginPage;
 import com.acti.pages.TaskListPage;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
 public class BaseTest extends DriverScript
 {
@@ -14,12 +18,25 @@ public class BaseTest extends DriverScript
 	LoginPage loginpage;
 	EnterTimePage entertimepage;
 	TaskListPage taskpage;
+	ExtentHtmlReporter htmlReport;
+	ExtentReports extent;
+	ExtentTest logger;
+	
+	
 
 	public BaseTest() 
 	   {
 		  super(); //Super keyword is used to call the parent class constructor
 		   
 	  }
+	@BeforeClass
+	public void reportconfig()
+	{
+		//create an object of EXTENDHTMLREPORTER and give the path of where reports to be generated
+		htmlReport = new ExtentHtmlReporter("./reports/Actiautomation.html");
+	    extent = new ExtentReports();
+	    extent.attachReporter(htmlReport);
+	}
 	
 	@BeforeMethod
     public void beforeTest()
@@ -30,10 +47,12 @@ public class BaseTest extends DriverScript
 	    taskpage = new TaskListPage();
 	   
     }
+	
 
      @AfterMethod
     public void tearDown()
     {
+    	 extent.flush();//which flush out the htmlreport(must to add on end when use ectenthtmlreporter)
     	driver.close();
     }
 }
--- src/test/java/com/acti/testcase/TestCreateCustomer.java
+++ src/test/java/com/acti/testcase/TestCreateCustomer.java
@@ -3,6 +3,8 @@ package com.acti.testcase;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.Status;
+
 
 
 public class TestCreateCustomer extends BaseTest
@@ -11,18 +13,29 @@ public class TestCreateCustomer extends BaseTest
 	  @Test
       public void TestCreateCustomer() throws Exception 
       {
+		  logger=extent.createTest("Test create New customer In TaskPage"); 
     	  loginpage.validateLoginFunction("admin","manager");
+    	  logger.log(Status.PASS,"Login was Successful");
     	  entertimepage.clickTaskmenu();
+    	  logger.log(Status.INFO,"clicked Task Menu");
     	  taskpage.clickaddnewbutton();
+    	  logger.log(Status.INFO,"Clicked add Newbutton");
     	  taskpage.clicknewcustomer();
+    	  logger.log(Status.INFO,"Clicked new customer");
     	  taskpage.entername("Testing customer");
+    	  logger.log(Status.INFO,"Entered customer name");
     	  taskpage.description("dummy message");
+    	  logger.log(Status.INFO,"Enter description");
     	  taskpage.create();
+    	  logger.log(Status.INFO,"Clicked on create customer button");
     	  String msg = taskpage.successmessage();
     	  System.out.println(msg);
     	  Assert.assertTrue(msg.contains("been created"));
+    	  logger.log(Status.PASS,"Customer Created Successfully");
     	  Thread.sleep(2000);
     	  entertimepage.clicklogout();
+    	  logger.log(Status.INFO,"Logged out from the Application");
+    	  
     	  
     	  
     	  
